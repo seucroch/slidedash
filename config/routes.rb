a391b55564654c730a9d46b01c1a44e70fcdde1d
seucroch <pablo.seuc.rocher@gmail.com>
2013-11-15 16:58:48 -0800

    Implemented Devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Slidedash::Application.routes.draw do
+
+  devise_for :users
+
   get "welcome/index"
 
   get "welcome/about"
 
-   root to:'welcome#index'
+  root to:'welcome#index'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
