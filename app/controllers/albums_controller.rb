5c2d7010d545722295fd4824e278ce021af5ccd6
seucroch <pablo.seuc.rocher@gmail.com>
2013-11-20 14:24:04 -0800

    edit an update for albums created

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -4,12 +4,37 @@ class AlbumsController < ApplicationController
   end
 
   def show
-     @albums = Album.find(params[:id])
+    @albums = Album.find(params[:id])
   end
 
   def new
+    @album = Album.new   
+  end
+
+  def create
+  @album = Album.new(params[:album])
+    
+    if @album.save
+      flash[:notice] = "Album was saved."
+      redirect_to @album
+    else
+      flash[:error] = "There was an error saving the Album. Please try again."
+      render :new
+    end
   end
 
   def edit
+    @album= Album.find(params[:id])
+  end
+
+  def update
+    @album = Album.find(params[:id])
+    if @album.update_attributes(params[:post])
+      flash[:notice] = "Album was updated."
+      redirect_to @album
+    else
+      flash[:error] = "There was an error saving the album. Please try again."
+      render :edit
+    end
   end
 end
