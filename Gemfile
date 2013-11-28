9916b46cd5eea474cc6f780a3d66d8ecf568eb87
seucroch <pablo.seuc.rocher@gmail.com>
2013-11-27 17:03:20 -0800

    workign swipe and modal with carrousel

--- Gemfile
+++ Gemfile
@@ -34,6 +34,7 @@ gem 'cancan'
 gem 'tumblr_client'
 gem 'figaro'
 gem 'tumblr-rb'
+gem 'puma'
 
 # To use ActiveModel has_secure_password
 # gem 'bcrypt-ruby', '~> 3.0.0'
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,20 +16,27 @@
 //= require_tree .
 
 $(document).ready(function() {
-  $(".js-swiper_viewer").click(function() {
+   $(".js-swiper_viewer").click(function() {
     var elem = document.getElementById('mySwipe');
-window.mySwipe = Swipe(elem, {
-   startSlide: 0,
-   speed: 300,
-   auto: 10000,
-   continuous: true,
-  // disableScroll: true,
-  // stopPropagation: true,
-  // callback: function(index, element) {},
-  // transitionEnd: function(index, element) {}
-});
-
-    return false;
+    window.mySwipe = Swipe(elem, {
+     startSlide: 0,
+     speed: 300,
+     auto: 1000,
+     continuous: true,
+     // disableScroll: true,
+     // stopPropagation: true,
+     // callback: function(index, element) {},
+     // transitionEnd: function(index, element) {}
+    });
+      return false;
   });
-});
+    $(".modal_viewer").click(function() {
+      $('#myModal').modal('show')
+         $('.carousel').carousel({
+            interval: 10000
+          })
+      return false;
+
+    });
+ });
 
